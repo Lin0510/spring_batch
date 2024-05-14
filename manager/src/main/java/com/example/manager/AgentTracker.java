4bbd01a07a0ffade05d63cbf4508fcc629c4bec9
林俞均 <anita.lin@promeritage.com.tw>
2024-05-14 10:01:10 +0800

    Annotation log

--- manager/src/main/java/com/example/manager/AgentTracker.java
+++ manager/src/main/java/com/example/manager/AgentTracker.java
@@ -1,8 +1,11 @@
 package com.example.manager;
 
+
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -18,13 +21,18 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
+@Slf4j
 public class AgentTracker {
-    private static final Logger log = LoggerFactory.getLogger(AgentTracker.class);
+    @Value("${agent.jar-path}")
+    private String agentJarPath;
     private Map<String, Process> runningProcesses = new ConcurrentHashMap<>();
     private Map<String, String> runningJobPort = new ConcurrentHashMap<>();
     private AtomicInteger port = new AtomicInteger(8081);
-    @Value("${agent.jar-path}")
-    private String agentJarPath;
+
+    @Bean
+    public Logger getLogger() {
+        return LoggerFactory.getLogger(AgentTracker.class);
+    }
 
     public String startAgent(String id) {
         Runtime.getRuntime().addShutdownHook(
